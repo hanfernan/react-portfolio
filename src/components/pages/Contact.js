7960e318cae53e35733b3af5c09518ea2eead4c4
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-05-19 17:24:24 -0400

    updated projects wrap

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,18 +7,18 @@ function Contact() {
     <div><h2>Contact Me!</h2></div>
       <div>
         <div>850.384.1535</div>
-        <div>
+        <div className=".contact-link">
           <i className="fas fa-paper-plane"></i>
           <a href="mailto:hfm11295@gmail.com" target="_blank">Email</a></div>
         <div>
           <i className="fab fa-github"></i>
-          <a href="https://github.com/hanfernan" target="_blank">GitHub</a>
+          <a href="https://github.com/hanfernan" target="_blank "className=".contact-link">GitHub</a>
         </div>
         <div>
           <i className="fab fa-linkedin"></i>
           <a
             href="https://www.linkedin.com/in/hannah-fernandes-martin/"
-            target="_blank"
+            target="_blank" className=".contact-link"
           >LinkedIn</a>
         </div>
       </div>
