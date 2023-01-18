bb2dab736c9331eca1535c39db00424b69bcecad
Hannah Fernandes-Martin <hannahfernandes-martin@Hannahs-MacBook-Pro-2.local>
2023-01-18 17:21:40 -0500

    removed comments

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,11 +3,9 @@ import { Link } from "react-router-dom";
 import Resume from '../../assets/Hannah_Fernandes-Martin_Resume23.pdf'
 import "./style.css";
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   const [click, setClick] = useState(false);
 
-  //reverses state of menu
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
   return (
